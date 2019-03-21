04926ca05e125d065d9301a962b3f2024768a53d
Dave Rix <dave@analysisbydesign.co.uk>
2019-03-21 23:04:16 +0000

    Add DynamoDB template build

---
+++ tf-templates/database/dynamodb/variables.tf
@@ -0,0 +1,23 @@
+# --------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# --------------------------------------------------------------------------------------------
+
+# Required -----------------------------------------------------------------------------------
+
+# DynamoDB Table Name
+variable "name" {
+  description = "Name of DynamoDB table"
+  type        = "string"
+}
+
+# Optional -----------------------------------------------------------------------------------
+
+variable "dynamodb_tags" {
+  description = "Additional tags for the DynamoDB table"
+  type        = "map"
+
+  default = {
+    "Component" = "dynamodb"
+  }
+}
