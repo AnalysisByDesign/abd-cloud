7cf078492dabcd5ea643b8305f0861efc7c7537b
Dave Rix <dave@analysisbydesign.co.uk>
2019-03-28 06:28:48 -0400

    Add admins remote role to account build

--- tf-templates/account/iam-roles.tf
+++ tf-templates/account/iam-roles.tf
@@ -6,6 +6,7 @@ resource "aws_iam_service_linked_role" "es" {
   aws_service_name = "es.amazonaws.com"
 }
 
+# --------------------------------------------------------------------------------------------
 # RDS Enhanced Monitoring Role Enabled at account level
 module "rds_monitoring_role" "rds_enhanced" {
   source = "git@github.com:AnalysisByDesign/abd-cloud-modules.git//security/iam-role"
@@ -15,5 +16,19 @@ module "rds_monitoring_role" "rds_enhanced" {
     description        = "Allows RDS enhanced monitoring"
     assume_role_policy = "${data.aws_iam_policy_document.rds_enhanced.json}"
     policy_arn         = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
+  }]
+}
+
+# --------------------------------------------------------------------------------------------
+# Administrator cross-account role access
+module "remote_admins" "this" {
+  source = "git@github.com:AnalysisByDesign/abd-cloud-modules.git//security/iam-role"
+
+  roles = [{
+    name               = "admins"
+    description        = "admins"
+    path               = "/"
+    assume_role_policy = "${data.aws_iam_policy_document.remote_assume_role_policy.json}"
+    policy_arn         = "arn:aws:iam::aws:policy/AdministratorAccess"
   }]
 }
