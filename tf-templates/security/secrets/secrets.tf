b3f4a0c02ce83b56eef74cbcc8151d802e2dbad9
Dave Rix <dave@analysisbydesign.co.uk>
2019-03-29 17:21:49 -0400

    Add initial secret storage

---
+++ tf-templates/security/secrets/secrets.tf
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------------------------
+# Prepare Secrets Manager
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Application RDS connectivity
+module "db-username" {
+  source = "git@github.com:AnalysisByDesign/abd-cloud-modules.git//security/secret-store"
+
+  required    = "${var.require_db ? 1 : 0}"
+  key         = "${local.vpc_name}/${var.component_name}/database/username"
+  value       = "db_username"
+  description = "The application RDS user name"
+  tags        = "${local.common_tags}"
+}
+
+module "db-password" {
+  source = "git@github.com:AnalysisByDesign/abd-cloud-modules.git//security/secret-store"
+
+  required    = "${var.require_db ? 1 : 0}"
+  key         = "${local.vpc_name}/${var.component_name}/database/password"
+  value       = "db_password"
+  description = "The application RDS user password"
+  tags        = "${local.common_tags}"
+}
---
+++ tf-templates/security/secrets/variables.tf
@@ -0,0 +1,22 @@
+# ============================================================================================
+# Common parameters
+# ============================================================================================
+
+# Required ----------------------
+
+variable "component_name" {
+  description = "Name of component to insert into secrets path /{vpc}/{component_name}/..."
+  type        = "string"
+}
+
+# Optional ----------------------
+
+variable "require_db" {
+  description = "Is a database username and password required"
+  default     = false
+}
+
+# ============================================================================================
+#                                      End of Variable Declarations
+# ============================================================================================
+
