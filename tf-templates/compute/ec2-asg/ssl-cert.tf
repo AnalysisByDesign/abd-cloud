c56a0f51546bd75f177b95dde63a820c5eab3ba6
Dave Rix <dave@analysisbydesign.co.uk>
2019-06-04 17:33:48 +0100

    Add wild-card cert to SSL

--- tf-templates/compute/ec2-asg/ssl-cert.tf
+++ tf-templates/compute/ec2-asg/ssl-cert.tf
@@ -11,7 +11,8 @@ module "ssl_cert" {
   acct_target_role   = "${var.acct_target_role}"
   certificate_region = "${var.target_region}"
 
-  domain_name = "${local.public_search_domain}"
+  domain_name               = "${local.public_search_domain}"
+  subject_alternative_names = ["*.${local.public_search_domain}"]
 
   r53_zone_id = "${data.aws_route53_zone.public.id}"
 
