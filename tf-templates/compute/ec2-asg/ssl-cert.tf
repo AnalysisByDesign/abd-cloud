902de8a8db2015d45f7aec4d1d1b7196c7b0f9d8
Dave Rix <dave@analysisbydesign.co.uk>
2019-06-28 00:01:19 +0100

    Final updates for v1 release

--- tf-templates/compute/ec2-asg/ssl-cert.tf
+++ tf-templates/compute/ec2-asg/ssl-cert.tf
@@ -18,3 +18,18 @@ module "ssl_cert" {
 
   common_tags = "${local.common_tags}"
 }
+
+module "ssl_cert_cloudfront" {
+  source = "git@github.com:AnalysisByDesign/abd-cloud-modules.git//security/ssl-certs"
+
+  required = true
+
+  acct_target        = "${var.acct_target}"
+  acct_target_role   = "${var.acct_target_role}"
+  certificate_region = "us-east-1"
+
+  domain_name               = "${local.public_search_domain}"
+  subject_alternative_names = ["*.${local.public_search_domain}"]
+  r53_zone_id               = "${data.aws_route53_zone.public.id}"
+  common_tags               = "${local.common_tags}"
+}
