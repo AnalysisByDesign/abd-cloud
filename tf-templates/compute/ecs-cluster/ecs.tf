de43ab613709d9e6b2a417e3d70c8b8d967ffb55
Dave Rix <dave@analysisbydesign.co.uk>
2019-10-23 21:30:02 +0100

    Tody up ECS usage

--- tf-templates/compute/ecs-cluster/ecs.tf
+++ tf-templates/compute/ecs-cluster/ecs.tf
@@ -1,15 +1,11 @@
-##########################################################################
-#
-# ECS MODULE
-#
-##########################################################################
+# -----------------------------------------------------------------------------
+# ECS Cluster
+# -----------------------------------------------------------------------------
 
 # Create cluster
 module "ecs-cluster" "this" {
-  #  source = "git@github.com:AnalysisByDesign/abd-cloud-modules.git//compute/ecs/cluster"
-  source = "../../../../abd-cloud-modules/compute/ecs/cluster"
+  source = "git@github.com:AnalysisByDesign/abd-cloud-modules.git//compute/ecs/cluster"
 
-  name = "${format("%s-%s", local.vpc_name, var.name)}"
-
+  name        = "${format("%s-%s", local.vpc_name, var.name)}"
   common_tags = "${local.common_tags}"
 }
